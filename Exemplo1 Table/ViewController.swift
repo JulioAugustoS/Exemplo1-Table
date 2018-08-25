625644ef8af9db2feea24983a57e4f12efb11a68
Usuário Convidado <Guest@L2602MICRO12.local>
2018-08-25 14:07:29 -0300

    Projeto 7

--- Exemplo1 Table/ViewController.swift
+++ Exemplo1 Table/ViewController.swift
@@ -8,10 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var meuTableView: UITableView!
+    
+    
+    var animais = ["Urso", "Girafa", "Elefante", "Rinoceronte", "Leão"]
+    // Não é obrigatorio informar a extenção da imagem
+    var fotos = ["urso.png", "girafa.png", "ele.png", "rino.png", "leao.png"]
+    var pais = ["Canadá", "Africa do Sul", "Zambia", "Índia", "Tanzania", "Sudão"]
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIImageView(image: UIImage(named: "cabecalho"))
+        
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footer = UIImageView(image: UIImage(named: "rodape"))
+        
+        return footer
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 70
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Você selecionou o \(animais[indexPath.row])")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animais.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        celula.textLabel?.text = animais[indexPath.row]
+        celula.detailTextLabel?.text = pais[indexPath.row]
+        celula.imageView?.image = UIImage(named: fotos[indexPath.row])
+        
+        return celula
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        meuTableView.dataSource = self
+        meuTableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
